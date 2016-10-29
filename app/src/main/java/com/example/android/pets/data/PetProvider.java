1a65f2e5c51dd494c83d69b652105e8c0d1bd94b
vsanjuan <salvador.sanjuan@gmail.com>
2016-10-29 15:50:55 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -0,0 +1,10 @@
+package com.example.android.pets.data;
+
+import android.content.ContentProvider;
+
+/**
+ * Created by salva on 29/10/2016.
+ */
+
+public class PetProvider extends ContentProvider {
+}
